c133d5beebe942819bf7c52abac659f95304c68f
Sgt. ScriptKiddy <nw.python.dev@gmail.com>
2017-09-11 13:38:18 -0700

    Update imgfap.py
    functions removed, added loop for multi downloads, prints downloaded picture file name

--- imgfap.py
+++ imgfap.py
@@ -2,32 +2,16 @@ import urllib.request
 import re
 import os
 
-URL = ""
-pnum = ""
-imglist = ""
-
-
-def mainLoop():
-    global URL
-    global pnum
-
+while True:
+    URL = ""
+    pnum = ""
+    imglist = ""
     URL = input("Please input the gallery url: ")
     url = "http://www.imagefap.com/pictures/"
     galleryId = URL.replace(url,"")
     galleryId = galleryId.split("/")[0]
     URL = "{}{}/?grid={}&view=2".format(url, galleryId, galleryId)
-    validate(URL)
-    
-def validate(URL):
-    print("Processing...")
-    try:
-        html =  urllib.request.urlopen(URL).read()
-    except Exception as e:
-            print("Unexpected Error: ", exc_info()[0], e)
-    getImages(html)
-
-def getImages(html):
-    global imglist
+    html =  urllib.request.urlopen(URL).read()
     imglist = re.findall('<td id="([0-9]+)" align="center"  ?valign="top">', str(html))
     imglist2 = []
     for image in imglist:
@@ -35,17 +19,11 @@ def getImages(html):
         html =  urllib.request.urlopen(url).read()
         imglist2.append(re.findall('"contentUrl": "(.*?)",', str(html)))
         print("Processed {}/{}".format(str(len(imglist2)), str(len(imglist))))
-    download(imglist2)
-    
-
-def download(urls):
+    urls = imglist2
     image = urls[0]
     image = image[0]
     dir_name = str(image).split("/")[-3]
-    try:
-        os.mkdir(dir_name)
-    except:
-        pass
+    os.mkdir(dir_name)
     print("Downloading {} images".format(str(len(urls))))
     for image in urls:
         image = str(image[0])
@@ -54,7 +32,7 @@ def download(urls):
             imageContent = f.read()
             with open(dir_name + '/' + name, "wb") as f:
                 f.write(imageContent)
-    
-        
-mainLoop()
-
+                print("downloaded " + name)
+    exit_in = input("download another?[y/n]")
+    if exit_in == "n":
+        exit()
